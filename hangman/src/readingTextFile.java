2c9bc36a372254213fc3f086bf853a0f52db1489
eejanak <23janakievskievan@bprep.org>
2021-05-17 09:53:25 -0700

    adding hangman text files

---
+++ hangman/src/readingTextFile.java
@@ -0,0 +1,7 @@
+import java.io.FileReader;
+
+public class readingTextFile {
+    public static void main(String[] args) {
+        FileReader reader = new FileReader("randomWordArrays.txt");
+    }
+}
