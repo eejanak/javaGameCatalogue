26015a21967913119725773de5c3d3ab4642c707
eejanak <23janakievskievan@bprep.org>
2021-05-31 13:41:58 -0700

    adding main index file. tidying up hangman

--- src/index.java
+++ src/index.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class index {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("Welcome to the game catalogue!\n");
+
+        playGame();
+    }
+
+    public static void playGame() {
+        String[] test = new String[0];
+
+        Scanner console = new Scanner(System.in);
+
+        System.out.println("Which game would you like to play?");
+        System.out.println("Type \"h\" for hangman, \"p\" for poker, and \"c\" for checkers.");
+
+        String game = console.nextLine();
+        if (game.equals("h")) {
+            hangman.main(test);
+        } else if (game.equals("p")) {
+
+        } else if (game.equals("c")) {
+
+        } else {
+            System.out.println("You did not pick a game properly.");
+            playGame();
+        }
     }
 }
